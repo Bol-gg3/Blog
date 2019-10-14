da278da566a6d8f4852e54a51cb8aedb2717b292
Jon Ander Aristimuño <jonander228@outlook.com>
2019-10-14 12:55:34 +0200

    prueba

---
+++ php/conector.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <!--Conectar a la base de datos-->
+<?php
+$contraseña = "2dw3123";
+$usuario = "grupo3";
+$nombre_base_de_datos = "grupo3";
+try{
+    $base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+    echo "conexión efectuada";
+}catch(Exception $e){
+	echo "Ocurrió algo con la base de datos: " . $e->getMessage();
+}
+?>
+</body>
+</html>
