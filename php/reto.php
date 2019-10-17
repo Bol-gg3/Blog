c140ee6c54322023636755a3ceb2425700be2b7e
Jon Ander Aristimuño <jonander228@outlook.com>
2019-10-17 08:44:39 +0200

    login emergente funcional

--- php/reto.php
+++ php/reto.php
@@ -7,15 +7,13 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>G3</title>
     <script>
-        /*window.onload=function(){
-            document.write("funciona");
-        }*/
+     
         function popupUploadForm(){
         var newWindow = window.open('../html/certificado.html', 'name', 'height=500,width=600');
     }
-    fuction login(){
+    function login1(){
         var newWindow = window.open('../html/login.html', 'name', 'height=500,width=600');
     }
     </script>
@@ -25,7 +23,7 @@ session_start();
         <form action="" method="POST">
         <label>Entra en Login si te has registrado sino registrate en Registro:
             <input type="submit" value="Registro" name="registro" onclick="popupUploadForm()">
-            <input type="submit" value="Login" name="login" id="login">
+            <input type="submit" value="Login" name="login" id="login" onclick="login1()">
             </label>
         </form>
     </header>
