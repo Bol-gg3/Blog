b5e62214864b1f24f6b61145aa294745336eacf3
Salomon Nzo <ik_2dw3@H015110.NET>
2019-10-21 12:29:32 +0200

    añadiendo la pagina de gestion de usuarios todo en uno

---
+++ php/comentario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comentario</title>
+</head>
+<body>
+
+<!--id_comentario 
+fecha_comentario 
+comentarios
+id_usuario -->
+    <?php
+    
+    if(isset($_POST["enviar"])){
+        echo $_POST['comentario'];
+        echo $_POST
+    }
+    ?>
+</body>
+</html>
