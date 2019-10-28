34ce9db248fa06ff49aeea7aa6513fb30aed809d
Lucian <Lucian2302@gmail.com>
2019-10-28 09:23:05 +0100

    Antes de arreglos

--- php/reto.php
+++ php/reto.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="../css.css">
+    <link rel="stylesheet" type="text/css" href="../newcss.css">
     <title>G3</title>
     <script>
         function popupUploadForm() {
@@ -201,7 +201,7 @@ function cambia() {
 
   console.log("1")
 
-  
+  var modo = "dia"
 
     if (modo ==="noche"){
         document.getElementById("logo").src = '../Recursos/logolight.jpg'
